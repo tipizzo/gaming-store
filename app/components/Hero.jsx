0df32af265392ce794e10a25bf661aeee4754390
KABANGA David <Davkabanga96@gmail.com>
2025-03-06 21:43:10 +0200

    setting up the hero section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,21 +1,31 @@
+'use client'
+
 import React from 'react';
 
 const Hero = () => {
+
     return (
         <div>
             {/* Mobile Version */}
             <div className='lg:hidden mx-6'>
-                <img src='/assets/mk_1.jpg' alt='Main Image' />
+                <img src='/assets/mk_1_hero.jpg' alt='Main Image' />
+                <div className='flex flex-col gap-2 mt-5'>
+                    <h1 className='font-bold text-3xl'>Gaming News</h1>
+                </div>
             </div>
 
             {/* Desktop Version */}
-            <div className='hidden lg:flex gap-10 mx-44 mt-5'>
-                <img src="/assets/mk_1.jpg" alt='Main Image' className='h-[800px] w-[1300px] rounded-xl' />
+
+            <div className='hidden lg:flex gap-10 mx-44 mt-10'>
+                    <div>
+                        <img src="/assets/mk_1_hero.jpg" alt='Main Image' className='h-[800px] w-[1300px] rounded-xl' />
+                    </div>
+                    <div>
+                        <img src="/assets/blackops_6.jpg" alt='Main Image' className='h-[800px] w-[1300px] rounded-xl' />
+                    </div>
+                <img src="/assets/mk_1_hero.jpg" alt='Main Image' className='h-[800px] w-[1300px] rounded-xl' />
                 <div className='flex flex-col gap-16'>
-                    <img src="/assets/scorpion.jpg" alt='Character' className='h-[150px] w-[250px] rounded-xl' />
-                    <img src="/assets/scorpion.jpg" alt='Character' className='h-[150px] w-[250px] rounded-xl' />
-                    <img src="/assets/scorpion.jpg" alt='Character' className='h-[150px] w-[250px] rounded-xl' />
-                    <img src="/assets/scorpion.jpg" alt='Character' className='h-[150px] w-[250px] rounded-xl' />
+                    <h1 className='font-bold text-3xl'>Gaming News</h1>
                 </div>
             </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import SearchSection from "./components/SearchSection";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default function Home() {
   return (
     <main>
