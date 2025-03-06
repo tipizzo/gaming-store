4d9c75ad6d4402ee0b9f16b52b4717e35eab9c45
KABANGA David <Davkabanga96@gmail.com>
2025-03-06 22:29:38 +0200

    Finalizing the hero section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,6 +1,12 @@
 'use client'
 
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/effect-fade'
+import {EffectFade, Autoplay, Pagination } from 'swiper/modules';
 
 const Hero = () => {
 
@@ -16,15 +22,34 @@ const Hero = () => {
 
             {/* Desktop Version */}
 
-            <div className='hidden lg:flex gap-10 mx-44 mt-10'>
-                    <div>
-                        <img src="/assets/mk_1_hero.jpg" alt='Main Image' className='h-[800px] w-[1300px] rounded-xl' />
-                    </div>
-                    <div>
-                        <img src="/assets/blackops_6.jpg" alt='Main Image' className='h-[800px] w-[1300px] rounded-xl' />
-                    </div>
-                <img src="/assets/mk_1_hero.jpg" alt='Main Image' className='h-[800px] w-[1300px] rounded-xl' />
-                <div className='flex flex-col gap-16'>
+            <div className='hidden lg:flex gap-5 ml-44 mt-10'>
+                <Swiper
+                    spaceBetween={30}
+                    effect={'fade'}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true
+                    }}
+                    modules={[EffectFade, Autoplay, Pagination]}
+                >
+                    <SwiperSlide>
+                        <img src="/assets/blackops_6.jpg" alt='Main Image' className='h-[650px] w-[1150px] rounded-xl' />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/assets/mk_1_hero.jpg" alt='Main Image' className='h-[650px] w-[1150px] rounded-xl' />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/assets/gta_5.jpg" alt='Main Image' className='h-[650px] w-[1150px] rounded-xl' />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/assets/tekken_7.jpg" alt='Main Image' className='h-[650px] w-[1150px] rounded-xl' />
+                    </SwiperSlide>
+                </Swiper>
+
+                <div className='flex flex-col justify-start w-full mr-16'>
                     <h1 className='font-bold text-3xl'>Gaming News</h1>
                 </div>
             </div>
--- app/page.js
+++ app/page.js
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import SearchSection from "./components/SearchSection";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
   return (
