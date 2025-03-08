4aa2b7004964b0d27b91d5724d316d1311b0bfd0
KABANGA David <Davkabanga96@gmail.com>
2025-03-08 04:09:56 +0200

    Adding HD picture for the card

--- app/components/GameCard.jsx
+++ app/components/GameCard.jsx
@@ -7,10 +7,10 @@ const GameCard = () => {
             <h1 className='flex items-center gap-2 font-bold text-2xl'>Discover Something New <ChevronRightIcon className='h-7 w-7' /></h1>
             <div className='flex flex-col gap-2 cursor-pointer'>
                 <div>
-                    <img src='/assets/far-cry-xx.jpg' alt='' className='h-[300px] w-[250px] rounded-lg mb-2' />
-                    <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Base Game</span>
+                    <img src='/assets/far-cry-2.webp' alt='' className='h-[300px] w-[250px] rounded-lg mb-2' />
+                    <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
                 </div>
-                <h3 className='font-semibold text-xl'>FAR CRY 5</h3>
+                <h3 className='font-semibold text-xl'>FAR CRY 2</h3>
                 <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Now on KGStore</span>
                 <span className='font-semibold'>Free</span>
             </div>
