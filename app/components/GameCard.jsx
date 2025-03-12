b5adb3a4152846fb8d8ff31ebb2650fc1b548f85
KABANGA David <Davkabanga96@gmail.com>
2025-03-12 15:39:04 +0200

    Refactoring the Game Card component

--- app/components/GameCard.jsx
+++ app/components/GameCard.jsx
@@ -1,112 +1,26 @@
 import React from 'react'
 import { ChevronRightIcon } from '@heroicons/react/24/outline'
+import games from '@/lib/GamesData'
 
 const GameCard = () => {
     return (
         <div className='ml-44 mt-10 flex flex-col gap-5'>
             <h1 className='flex items-center gap-2 font-bold text-2xl'>Discover Something New <ChevronRightIcon className='h-7 w-7' /></h1>
             <div className='flex  gap-8 game-card'>
-                
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
-                <div className='flex flex-col gap-2 cursor-pointer'>
-                    <div>
-                        <img src='/assets/wukong.png' alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
-                        <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>Action & Adventure</span>
-                    </div>
-                    <h3 className='font-semibold text-xl'>BLACK MYTH: WUKONG</h3>
-                    <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>Available</span>
-                    <span className='font-semibold'>Coming Soon</span>
-                </div>
+                {games.map((item, index) => {
+                    return (
+                        <div key={index} className='flex flex-col gap-2 cursor-pointer'>
+                            <div>
+                                <img src={item.image} alt='' className='h-[300px] w-[200px] rounded-lg mb-2' />
+                                <span className='text-gray-400 font-bold text-sm px-1 rounded-xl'>{item.category}</span>
+                            </div>
+                            <h3 className='font-semibold text-xl'>{item.title}</h3>
+                            <span className='font-bold bg-gray-600 text-gray-300 w-max px-2 rounded-lg'>{item.availability}</span>
+                            <span className='font-semibold'>{item.price}</span>
+                        </div>
+                    )
+                })}
+
             </div>
         </div>
     )
